fe76f59a1ad8f1242323260ff46b19c6a45a7753
Stephen Chau <chtzch0713@gmail.com>
2023-11-09 21:27:57 -0800

    updt

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,52 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', event => {
-
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+ * Start Bootstrap - Stylish Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+ */
+window.addEventListener("DOMContentLoaded", (event) => {
+    const sidebarWrapper = document.getElementById("sidebar-wrapper");
     let scrollToTopVisible = false;
     // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
+    const menuToggle = document.body.querySelector(".menu-toggle");
+    menuToggle.addEventListener("click", (event) => {
         event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
+        sidebarWrapper.classList.toggle("active");
         _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
+        menuToggle.classList.toggle("active");
+    });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
-    scrollTriggerList.map(scrollTrigger => {
-        scrollTrigger.addEventListener('click', () => {
-            sidebarWrapper.classList.remove('active');
-            menuToggle.classList.remove('active');
+    var scrollTriggerList = [].slice.call(
+        document.querySelectorAll("#sidebar-wrapper .js-scroll-trigger")
+    );
+    scrollTriggerList.map((scrollTrigger) => {
+        scrollTrigger.addEventListener("click", () => {
+            sidebarWrapper.classList.remove("active");
+            menuToggle.classList.remove("active");
             _toggleMenuIcon();
-        })
+        });
     });
 
     function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
+        const menuToggleBars = document.body.querySelector(
+            ".menu-toggle > .fa-bars"
+        );
+        const menuToggleTimes = document.body.querySelector(
+            ".menu-toggle > .fa-xmark"
+        );
         if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-xmark');
+            menuToggleBars.classList.remove("fa-bars");
+            menuToggleBars.classList.add("fa-xmark");
         }
         if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-xmark');
-            menuToggleTimes.classList.add('fa-bars');
+            menuToggleTimes.classList.remove("fa-xmark");
+            menuToggleTimes.classList.add("fa-bars");
         }
     }
 
     // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
+    document.addEventListener("scroll", () => {
+        const scrollToTop = document.body.querySelector(".scroll-to-top");
         if (document.documentElement.scrollTop > 100) {
             if (!scrollToTopVisible) {
                 fadeIn(scrollToTop);
@@ -53,28 +58,64 @@ window.addEventListener('DOMContentLoaded', event => {
                 scrollToTopVisible = false;
             }
         }
-    })
-})
+    });
+
+    var portfolioItems = document.querySelectorAll(".portfolio-item");
+
+    portfolioItems.forEach(function (item) {
+        item.addEventListener("click", function () {
+            // Get data from the clicked portfolio item
+            var projectName = this.querySelector(".h1").textContent;
+
+            // Set modal title
+            document.getElementById("projectModalLabel").textContent =
+                projectName;
+
+            // Set modal content based on the clicked item
+            var projectContent = getProjectContent(projectName);
+            document.getElementById("projectModalBody").innerHTML =
+                projectContent;
+
+            // Show the modal
+            $("#projectModal").modal("show");
+        });
+    });
+
+    function getProjectContent(projectName) {
+        switch (projectName) {
+            case "Jchannel":
+                return "<p>Details about Jchannel project.</p>";
+            case "Devlytics":
+                return "<p>Details about Devlytics project.</p>";
+            case "AI tools":
+                return "<p>Details about AI tools project.</p>";
+            case "BirdNET Research":
+                return "<p>Details about BirdNET Research project.</p>";
+            default:
+                return "<p>No details available.</p>";
+        }
+    }
+});
 
 function fadeOut(el) {
     el.style.opacity = 1;
     (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
+        if ((el.style.opacity -= 0.1) < 0) {
             el.style.display = "none";
         } else {
             requestAnimationFrame(fade);
         }
     })();
-};
+}
 
 function fadeIn(el, display) {
     el.style.opacity = 0;
     el.style.display = display || "block";
     (function fade() {
         var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
+        if (!((val += 0.1) > 1)) {
             el.style.opacity = val;
             requestAnimationFrame(fade);
         }
     })();
-};
+}
